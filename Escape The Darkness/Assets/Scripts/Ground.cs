c528f5df3afe889701ba97d07866b6bc5c355582
AC215888 <AC215888@FALMOUTH.AC.UK>
2020-05-11 13:45:50 +0100

    Organising files

--- Escape The Darkness/Assets/Scripts/Ground.cs
+++ Escape The Darkness/Assets/Scripts/Ground.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] groundPrefabs;
+
+    private Transform playerTransform;
+    private float spawnZ = 115.0f;
+    private float groundLength = 5.0f;
+    private int amnGroundOnScreen = 10;
+
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        SpawnGround();
+        SpawnGround();
+        SpawnGround();
+        SpawnGround();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnGround (int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(groundPrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += groundLength;
     }
 }
